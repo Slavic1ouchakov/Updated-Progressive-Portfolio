983b8944f3584b5d3f755044fbf111f4a3669bb4
Slavic1ouchakov <slavic.ouchakov@gmail.com>
2021-07-28 21:34:39 -0400

    added form fuctionality

---
+++ Assets/js/index.js
@@ -0,0 +1,39 @@
+document.getElementById("send").addEventListener("click", validateForm);
+function validateForm() {
+  //gets the first name
+  let name = document.getElementById("fname").value;
+  //gets the last name
+  let name = document.getElementById("lname").value;
+  //gets the email
+  let email = document.getElementById("email").value;
+  //gets the message
+  let message = document.getElementById("subject").value;
+
+  //checks if all fields have been filled before sending message.
+  if (name.trim() == "" || email.trim() == "" || message.trim() == "") {
+    alert("all fields must be filled");
+  } else {
+    sendMessage(name, email, message);
+  }
+}
+function sendMessage(fname, lname, email, subject) {
+  //sends the name, email and message by passing them as url parameters
+  window.location.href =
+    "https://us-central1-naishare.cloudfunctions.net/sendMail?name=" +
+    fname +
+    "&email=" +
+    lname +
+    "&email=" +
+    email +
+    "&message=" +
+    subject +
+    "";
+}
+
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
